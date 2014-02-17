5c439e2dd0eaaf52d050bb914367b5cc1aaf422f
Dag Ivarsoy <dagivarsoy@gmail.com>
2014-02-17 08:55:08 +0000

    Opprettet prosjekt klasse og side.

---
+++ kode/SysUt14Gr03/SysUt14Gr03.Tests/prosjektTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SysUt14Gr03.Classes;
+
+namespace SysUt14Gr03.Tests
+{
+    [TestClass]
+    public class prosjektTest
+    {
+        [TestMethod]
+        public void TestSetteNavnPaaProsjektet()
+        {
+            Prosjekt prosjekt = new Prosjekt();
+
+            prosjekt.Name = "Blaa Himmel";
+
+            string expected = "Blaa Himmel";
+
+            Assert.AreEqual(expected, prosjekt.Name);
+        }
+    }
+}
---
+++ kode/SysUt14Gr03/SysUt14Gr03/Prosjekt.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SysUt14Gr03
+{
+    public partial class Prosjekt : System.Web.UI.Page
+    {
+        Prosjekt prosjekt;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            prosjekt = new Prosjekt();
+            //prosjekt.Navn = "Første prosjektet";
+        }
+    }
+}
