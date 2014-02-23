e7af79c274c62108af3d3de5dbceb2dfd0403ca7
Andreas D Jansson <drknert@gmail.com>
2014-02-23 17:53:29 +0100

    Lagt til login.aspx

---
+++ kode/SysUt14Gr03/SysUt14Gr03/Login.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="SysUt14Gr03.Logon" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" Runat="Server">
+    <h1>
+        Logg inn</h1>
+    <p>
+        E-postadresse:
+        <asp:TextBox ID="UserName" runat="server"></asp:TextBox></p>
+    <p>
+        Passord:
+        <asp:TextBox ID="Password" runat="server" TextMode="Password"></asp:TextBox></p>
+    <p>
+        <asp:CheckBox ID="RememberMe" runat="server" Text="Husk meg" /> </p>
+    <p>
+        <asp:Button ID="LoginButton" runat="server" Text="Logg inn" OnClick="LoginButton_Click" /> </p>
+    <p>
+        <asp:Label ID="InvalidCredentialsMessage" runat="server" ForeColor="Red" Text="Feil brukernavn eller passord"
+            Visible="False"></asp:Label> </p>
+</asp:Content>
---
+++ kode/SysUt14Gr03/SysUt14Gr03/Login.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SysUt14Gr03
+{
+    public partial class Logon : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void LoginButton_Click(object sender, EventArgs e)
+        {
+            // Dummybrukere
+            // Skal hente detaljer fra database
+            string[] users = { "Arne", "Per", "Pål" };
+            string[] passwords = { "12345", "qwerty", "password" };
+            for (int i = 0; i < users.Length; i++)
+            {
+                bool validUsername = (string.Compare(UserName.Text, users[i], true) == 0);
+                bool validPassword = (string.Compare(Password.Text, passwords[i], false) == 0);
+                if (validUsername && validPassword)
+                {
+                    // Logg inn bruker
+                    Response.Redirect("brukere.aspx", true);
+                }
+            }
+            // Feil brukernavn eller passord
+            InvalidCredentialsMessage.Visible = true;
+        }
+    }
+}
