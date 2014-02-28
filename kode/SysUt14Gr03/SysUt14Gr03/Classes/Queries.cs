c22a9fdff2da91f29de9ef167a4192bf00408b93
Dag Ivarsoy <dagivarsoy@gmail.com>
2014-02-28 23:45:26 +0000

    Administrasjon av Rettigheter Ferdig

--- kode/SysUt14Gr03/SysUt14Gr03/Classes/Queries.cs
+++ kode/SysUt14Gr03/SysUt14Gr03/Classes/Queries.cs
@@ -6,7 +6,7 @@ using SysUt14Gr03.Models;
 
 namespace SysUt14Gr03.Classes
 {
-    static public class Queries
+    public class Queries
     {
         static public Bruker GetBruker(int _bruker_id)
         {
@@ -28,5 +28,23 @@ namespace SysUt14Gr03.Classes
             
             }
         }
+
+        /*
+        static public List<Rettighet> GetAlleRettigheter()
+        {
+            using (var context = new Context())
+            {
+                return context.Rettigheter.ToList<Rettighet>();
+            }
+        }
+        */
+        /*
+        public static IQueryable<Rettighet> GetAlleRettigheter()
+        {
+            var context = new Context();
+            IQueryable<Rettighet> query = context.Rettigheter;
+            return query;
+        }
+        */
     }
 }
