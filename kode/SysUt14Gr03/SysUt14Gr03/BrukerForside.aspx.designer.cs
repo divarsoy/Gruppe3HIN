cc8dbe9caf776314b42368d50c3654c2653f715a
sitha93 <lillesith@gmail.com>
2014-03-01 19:01:22 +0100

    designet forside

--- kode/SysUt14Gr03/SysUt14Gr03/BrukerForside.aspx.designer.cs
+++ kode/SysUt14Gr03/SysUt14Gr03/BrukerForside.aspx.designer.cs
@@ -11,5 +11,32 @@ namespace SysUt14Gr03 {
     
     
     public partial class BrukerForside {
+        
+        /// <summary>
+        /// tempButton control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button tempButton;
+        
+        /// <summary>
+        /// Calendar1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Calendar Calendar1;
+        
+        /// <summary>
+        /// Image1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image Image1;
     }
 }
