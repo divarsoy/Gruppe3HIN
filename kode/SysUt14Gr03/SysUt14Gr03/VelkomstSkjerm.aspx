e054e75aa69887516e7ca26a436558dfe6e93728
Frederik Johnsen <fredrik_johnsen92@hotmail.com>
2014-03-01 20:27:40 +0100

    velkomstskjermen e søppel, slettes.

---
+++ kode/SysUt14Gr03/SysUt14Gr03/VelkomstSkjerm.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Title="Contact" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="VelkomstSkjerm.aspx.cs" Inherits="SysUt14Gr03.VelkomstSkjerm" %>
+
+<asp:Content ID="Content1" runat="server" contentplaceholderid="MainContent">
+   
+    <p>
+
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;Søk etter medlemmer i din gruppe:&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+
+    </p>
+    <p>
+        &nbsp;</p>
+            <asp:Calendar ID="Calendar1" runat="server" BackColor="White" BorderColor="White" BorderWidth="1px" Font-Names="Verdana" Font-Size="9pt" ForeColor="Black" Height="190px" NextPrevFormat="FullMonth" Width="1030px">
+                <DayHeaderStyle Font-Bold="True" Font-Size="8pt" />
+                <NextPrevStyle Font-Bold="True" Font-Size="8pt" ForeColor="#333333" VerticalAlign="Top" />
+                <OtherMonthDayStyle ForeColor="#999999" />
+                <SelectedDayStyle BackColor="#333399" ForeColor="White" />
+                <TitleStyle BackColor="White" BorderColor="Black" BorderWidth="4px" Font-Bold="True" Font-Size="12pt" ForeColor="#333399" />
+                <TodayDayStyle BackColor="#CCCCCC"/>
+            </asp:Calendar>
+    <div>
+        <asp:Panel ID="Panel1" runat="server" Height="537px" style="text-align: center">
+
+             <asp:Panel ID="Meldinger" runat="server" Height="200px" Width="327px">
+                 <asp:Button ID="Button1" runat="server" Text="Button" />
+             </asp:Panel>
+            <asp:Panel ID="Notifikasjon" runat="server" Height="200px" Width="530px" style="text-align: center">
+                 <asp:Button ID="Button2" runat="server" Text="Button" />
+             </asp:Panel>
+            <asp:Panel ID="Oppgaver" runat="server" Height="200px" Width="439px">
+                 <asp:Button ID="Button3" runat="server" Text="Button" />
+             </asp:Panel>
+        </asp:Panel>
+    </div>
+    
+ 
+</asp:Content>
+
+
--- kode/SysUt14Gr03/SysUt14Gr03/changeUser.aspx.cs
+++ kode/SysUt14Gr03/SysUt14Gr03/changeUser.aspx.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data.SqlClient;
@@ -11,9 +12,16 @@ namespace SysUt14Gr03
 {
     public partial class changeUser : System.Web.UI.Page
     {
+        private int userID = 1;
+        private string oldDBPassword = "johnsen";
+        private lostPassword updatePassord;
+        private string newPassword;
+        private string oldPassword;
+        private string confirmPassword;
+
         protected void Page_Load(object sender, EventArgs e)
         {
-            
+            updatePassord = new lostPassword();
         }
         protected void btnPasswordChange_Click(object sender, EventArgs e)
         {
@@ -21,27 +29,72 @@ namespace SysUt14Gr03
         }
         protected void btnUserChange_Click(object sender, EventArgs e)
         {
-            int id = 0;
             string firstName = FirstName.Text.Trim();
             string surName = SurName.Text.Trim();
             string userName = UserName.Text.Trim();
             string email = Email.Text.Trim();
             string im = IM.Text.Trim();
-            string oldPassword = OldPassord.Text.Trim();
-            string newPassword = NewPassword.Text.Trim();
-            string confirmPassword = ConfirmPassword.Text.Trim();
 
-            updateUser(email, firstName, surName, id, im, userName, newPassword);
+            updateUser(email, firstName, surName, userID, im, userName);
+
+            if(HiddenPanel.Visible)
+            {
+                oldPassword = OldPassord.Text.Trim();
+                newPassword = NewPassword.Text.Trim();
+                confirmPassword = ConfirmPassword.Text.Trim();
+                
+                if(oldDBPassword == oldPassword && newPassword == confirmPassword)
+                    updatePassord.updatePassword(email, newPassword);
+            }
         }
-        public void updateUser(string email,string firstnavn, string surName, int id, string im, string userName, string password)
+        public void updateUser(string email,string firstnavn, string surName, int id, string im, string userName)
         {
             using (SqlCommand command = new SqlCommand())
             {
                 command.Connection = new SqlConnection(ConfigurationManager.ConnectionStrings["sysUt14Gr03"].ConnectionString);
-                command.CommandText = @"update Bruker where Bruker_id='" + id + "' set Etternavn='" + surName + "' set ForNavn='" + firstnavn + "' set Brukernavn='" + userName + "' set Epost='" + email + "' set IM='" + im + "' set Passord=" + password + "'";
+                command.CommandText = @"update Bruker where Bruker_id='" + id + "' set Etternavn='" + surName + "' set ForNavn='" + firstnavn + "' set Brukernavn='" + userName + "' set Epost='" + email + "' set IM='" + im + "'";
                 command.Connection.Open();
                 command.ExecuteNonQuery();
                 command.Connection.Close();
+            }
+        }
+        public ArrayList getUserInfo()
+        {
+            using (SqlCommand command = new SqlCommand())
+            {
+                string query = "SELECT * FROM Bruker WHERE Bruker_id = " + userID + "'";
+                command.Connection = new SqlConnection(ConfigurationManager.ConnectionStrings["sysUt14Gr03"].ConnectionString);
+
+                var list = new ArrayList();
+                var reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        string etterNavn = reader.GetString(1);
+                        string forNavn = reader.GetString(2);
+                        string brukerNavn = reader.GetString(3);
+                        string epost = reader.GetString(4);
+                        oldDBPassword = reader.GetString(5);
+                        string im = reader.GetString(6);
+                        list.Add(forNavn);
+                        list.Add(etterNavn);
+                        list.Add(brukerNavn);
+                        list.Add(epost);
+                        list.Add(oldDBPassword);
+                        list.Add(im);
+                    }
+                }
+                else
+                {
+                    string respons = "Fikk ikke hentet ut informasjon fra tabellen Bruker";
+                    list.Add(respons);
+                }
+
+                reader.Close();
+                command.Connection.Close();
+                return list;
             }
         }
     }
