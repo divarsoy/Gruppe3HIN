ad8bb737fe2c09a902079d3508d15f38d5146b34
Dag Ivarsoy <dagivarsoy@gmail.com>
2014-03-07 20:05:54 +0000

    La til nye ferdige oppgaver i menyen og publiserte

--- kode/SysUt14Gr03/SysUt14Gr03/Site.Master.designer.cs
+++ kode/SysUt14Gr03/SysUt14Gr03/Site.Master.designer.cs
@@ -20,23 +20,5 @@ namespace SysUt14Gr03 {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder MainContent;
-        
-        /// <summary>
-        /// Panel1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Panel Panel1;
-        
-        /// <summary>
-        /// Calendar1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Calendar Calendar1;
     }
 }
