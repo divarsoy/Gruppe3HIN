c5a19de3b14d4303371c4edadc39a8908d5346ca
Andreas D Jansson <drknert@gmail.com>
2014-03-21 13:25:06 +0100

    Oppdatert master-fila

--- kode/SysUt14Gr03/SysUt14Gr03/MottaOppgave.aspx.cs
+++ kode/SysUt14Gr03/SysUt14Gr03/MottaOppgave.aspx.cs
@@ -23,7 +23,7 @@ namespace SysUt14Gr03
         {
             if (Session["loggedIn"] == null)
             {
-                Response.Redirect("default.aspx", true);
+                Response.Redirect("Login.aspx", true);
             }
             else
             {
