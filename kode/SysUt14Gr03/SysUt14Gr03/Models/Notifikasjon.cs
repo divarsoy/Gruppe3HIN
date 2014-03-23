fce0a5461a389254f57f80ae66b0f407d793ac49
sitha93 <lillesith@gmail.com>
2014-03-23 16:41:06 +0100

    lagt til notifikasjoner i database

---
+++ kode/SysUt14Gr03/SysUt14Gr03/Models/Notifikasjon.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SysUt14Gr03.Models
+{
+    public class Notifikasjon
+    {
+        [Key]
+        public int Notifikasjon_id { get; set; }
+        public string Melding { get; set; }
+        public int NotifikasjonsType_id { get; set; }
+        public int Bruker_id { get; set; }
+        public bool Vist { get; set; }
+
+        public virtual Bruker bruker { get; set; }
+        public virtual NotifikasjonsType notifikasjonsType { get; set; }
+    }
+}
---
+++ kode/SysUt14Gr03/SysUt14Gr03/Models/NotifikasjonsType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SysUt14Gr03.Models
+{
+    public class NotifikasjonsType
+    {
+        [Key]
+        public int NotifikasjonsType_id { get; set; }
+        public string Type { get; set; }
+    }
+}
