acc920f96ddcbbba66200ba91326d51ae608f2fc
eivindjs <eivind.skreddernes@gmail.com>
2014-04-05 15:16:18 +0200

    Ordnet sjekk på brukernavn!

--- kode/SysUt14Gr03/SysUt14Gr03/AktiverKonto.aspx.designer.cs
+++ kode/SysUt14Gr03/SysUt14Gr03/AktiverKonto.aspx.designer.cs
@@ -13,6 +13,15 @@ namespace SysUt14Gr03 {
     public partial class AktiverKonto {
         
         /// <summary>
+        /// lblAktivert control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblAktivert;
+        
+        /// <summary>
         /// lblUsername control.
         /// </summary>
         /// <remarks>
@@ -29,6 +38,15 @@ namespace SysUt14Gr03 {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox Username;
+        
+        /// <summary>
+        /// lblBrukernavnFeil control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblBrukernavnFeil;
         
         /// <summary>
         /// lblAftername control.
