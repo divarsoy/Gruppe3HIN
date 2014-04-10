ae0b86299fab54d1c11dab6c7e314207ef2aa173
sitha93 <lillesith@gmail.com>
2014-04-10 09:27:55 +0200

    lagt til klassene for innsyn

---
+++ kode/SysUt14Gr03/SysUt14Gr03/Utvikler/InnsynIEgneRegistrerteTimerSomBruker.aspx
@@ -0,0 +1,10 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Utvikler.master" AutoEventWireup="true" CodeBehind="InnsynIEgneRegistrerteTimerSomBruker.aspx.cs" Inherits="SysUt14Gr03.Utvikler.InnsynIEgneRegistrerteTimerSomBruker" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="LogoTemplate" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MenyTemplate" runat="server">
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="table">
+    <asp:PlaceHolder ID="PlaceHolderTable" runat="server"></asp:PlaceHolder>
+    </div>
+</asp:Content>
---
+++ kode/SysUt14Gr03/SysUt14Gr03/Utvikler/InnsynIEgneRegistrerteTimerSomBruker.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using SysUt14Gr03.Models;
+using SysUt14Gr03.Classes;
+
+namespace SysUt14Gr03.Utvikler
+{
+    public partial class InnsynIEgneRegistrerteTimerSomBruker : System.Web.UI.Page
+    {
+        int brukerId = 2;
+        Prosjekt prj = new Prosjekt();
+        List<Time> timer;
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            timer = Queries.GetTimerForBruker(brukerId);
+            prj.Navn = "EksempelProsjekt";
+
+                if (!IsPostBack)
+                {
+                    Table timeTabell = Tabeller.HentTimerForBruker(timer, brukerId, prj);
+
+                    PlaceHolderTable.Controls.Add(timeTabell);
+                }
+            
+            
+        }
+    }
+}
