a3c86a0cd28b4934d3598ccd4f07564efb408831
gerr8ard <pskogsru88@hotmail.com>
2014-04-27 22:28:10 +0200

    Eksport til excel klasse og datatabell klasse

---
+++ kode/SysUt14Gr03/SysUt14Gr03/Classes/DataTabeller.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using SysUt14Gr03.Models;
+
+namespace SysUt14Gr03.Classes
+{
+    public class DataTabeller
+    {
+
+        public static DataTable OversiktBrukere(List<Bruker> query)
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add(new DataColumn("bruker_id", typeof(System.Int32)));
+            dt.Columns.Add(new DataColumn("Fornavn", typeof(System.String)));
+            dt.Columns.Add(new DataColumn("Etternavn", typeof(System.String)));
+
+            foreach (Bruker bruker in query)
+            {
+                DataRow row = dt.NewRow();
+                row["bruker_id"] = bruker.Bruker_id;
+                row["Fornavn"] = bruker.Fornavn;
+                row["Etternavn"] = bruker.Etternavn;
+
+                dt.Rows.Add(row);
+
+            }
+            return dt;
+        } 
+
+    }
+}
