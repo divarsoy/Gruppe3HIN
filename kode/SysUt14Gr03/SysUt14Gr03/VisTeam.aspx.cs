945b455625244f7918153f440b42d6ac37557e71
Dag Ivarsoy <dagivarsoy@gmail.com>
2014-04-30 11:08:20 +0100

    Små justeringer av layout på VisTeam

--- kode/SysUt14Gr03/SysUt14Gr03/VisTeam.aspx.cs
+++ kode/SysUt14Gr03/SysUt14Gr03/VisTeam.aspx.cs
@@ -36,7 +36,7 @@ namespace SysUt14Gr03
             {
                 prosjekt_id = Validator.KonverterTilTall(Session["prosjekt_id"].ToString());
                 Prosjekt prosjekt = Queries.GetProsjekt(prosjekt_id);
-                lblProsjekt.Text = string.Format("<h1>Valgt Prosjekt: {0}</h1>", prosjekt.Navn); 
+                lblProsjekt.Text = string.Format("Prosjekt: {0}", prosjekt.Navn); 
                 team = Queries.GetTeamByProsjekt(prosjekt_id);
                 teamId = team.Team_id;
             }
