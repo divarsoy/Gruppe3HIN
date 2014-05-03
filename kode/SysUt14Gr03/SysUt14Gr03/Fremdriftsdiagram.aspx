c175996e468340ad1cfe1de8f87b7c912a2836cc
Frederik <fredrik_johnsen92@hotmail.com>
2014-05-03 12:42:47 +0200

    fremdriftsdiagram nesten ferdig

---
+++ kode/SysUt14Gr03/SysUt14Gr03/Fremdriftsdiagram.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Prosjektleder.master" AutoEventWireup="true" CodeBehind="Fremdriftsdiagram.aspx.cs" Inherits="SysUt14Gr03.Fremdriftsdiagram" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <h1>Fremdriftsdiagram</h1>
+    <asp:DropDownList ID="ddlFaser" runat="server" OnSelectedIndexChanged="ddlFaser_SelectedIndexChanged" AutoPostBack="true"></asp:DropDownList>
+    <asp:Panel ID="Chart" runat="server"></asp:Panel>
+    <asp:Chart ID="ChartHolder" runat="server" Width="800px" BackColor="LightGray" Height="350px">
+        <Series>
+            <asp:Series ChartType="Line" Name="Brukte tid" YValuesPerPoint="2"></asp:Series>
+            <asp:Series ChartArea="ChartArea1" ChartType="Line" Name="Estimert tid" YValuesPerPoint="2"></asp:Series>
+        </Series>
+        <ChartAreas>
+            <asp:ChartArea Name="ChartArea1">
+                <AxisY Title="Estimert tid - brukt tid"></AxisY>
+            </asp:ChartArea>
+        </ChartAreas>
+    </asp:Chart>
+    <asp:Label ID="hei" runat="server"></asp:Label>
+</asp:Content>
+
---
+++ kode/SysUt14Gr03/SysUt14Gr03/Fremdriftsdiagram.aspx.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.DataVisualization.Charting;
+using SysUt14Gr03.Classes;
+using SysUt14Gr03.Models;
+
+namespace SysUt14Gr03
+{
+    public partial class Fremdriftsdiagram : System.Web.UI.Page
+    {
+        private int faseID;
+        private int prosjektID;
+
+        private TimeSpan estimat;
+        private TimeSpan bruktTid;
+        private TimeSpan tidIgjen;
+        private DateTime startDato;
+        private DateTime sluttDato;
+
+        private Fase fase;
+        private List<Fase> faser;
+        private List<Oppgave> oppgaver;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //SessionSjekk.sjekkForProsjekt_id();
+            //SessionSjekk.sjekkForBruker_id();
+
+            if (!IsPostBack)
+            {
+                tidIgjen = new TimeSpan();
+                estimat = new TimeSpan();
+                bruktTid = new TimeSpan();
+
+                prosjektID = 1;// Validator.KonverterTilTall(Session["prosjekt_id"].ToString());
+                faser = Queries.GetFaseForProsjekt(prosjektID);
+
+                for (int i = 0; i < faser.Count; i++)
+                    ddlFaser.Items.Add(new ListItem(faser[i].Navn, faser[i].Fase_id.ToString()));
+
+                this.fillGraph();
+            }
+        }
+
+        protected void ddlFaser_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.fillGraph();
+        }
+        private void fillGraph()
+        {
+            faseID = Convert.ToInt32(ddlFaser.SelectedValue);
+            fase = Queries.GetFase(faseID);
+
+            oppgaver = Queries.getOppgaverIFase(faseID);
+
+            for (int i = 0; i < oppgaver.Count; i++)
+            {
+                estimat += (TimeSpan)oppgaver[i].Estimat;
+                bruktTid += (TimeSpan)oppgaver[i].BruktTid;
+            }
+            // Diagramm egenskaper
+            this.ChartHolder.ChartAreas["ChartArea1"].AxisX.Interval = 1; // makes sure each point has a label
+            this.ChartHolder.Series["Brukte tid"].BorderWidth = 3; // changes the width of the burn-up-line
+            this.ChartHolder.Series["Estimert tid"].BorderWidth = 3; // changes the width of the burn-up-ceiling
+            this.ChartHolder.Series["Estimert tid"].Color = System.Drawing.Color.Red; // changes the color of the burn-up-ceiling to red
+            this.ChartHolder.ChartAreas["ChartArea1"].AxisX.LabelStyle.Angle = -45;
+            this.ChartHolder.ChartAreas["ChartArea1"].Area3DStyle.Enable3D = false;
+            this.ChartHolder.Legends.Add(new Legend("Legend"));
+            this.ChartHolder.Legends["Legend"].Enabled = true;
+
+            startDato = Convert.ToDateTime(fase.Start);
+            sluttDato = Convert.ToDateTime(fase.Stopp);
+
+            List<DateTime> range = Enumerable.Range(0, (sluttDato - startDato).Days + 1)
+                .Select(i => startDato.AddDays(i))
+                .ToList();
+
+            tidIgjen = estimat;
+            TimeSpan h = new TimeSpan();
+            foreach (var d in range)
+            {
+                for (int i = 0; i < oppgaver.Count; i++)
+                {
+                    if ((TimeSpan)oppgaver[i].BruktTid > (TimeSpan)oppgaver[i].Estimat)
+                    {
+                        h = tidIgjen + ((TimeSpan)oppgaver[i].BruktTid - (TimeSpan)oppgaver[i].Estimat);
+                    }
+
+                    if (d.Date == oppgaver[i].Startet.Value.Date && oppgaver[i].Status_id == 3)
+                    {
+                        tidIgjen = tidIgjen - (TimeSpan)oppgaver[i].Estimat;
+                    }
+                    else if (d.Date == oppgaver[i].Startet.Value.Date)
+                    {
+                        tidIgjen = tidIgjen - (TimeSpan)oppgaver[i].BruktTid;
+                    }
+                }
+                
+                if(h != null)
+                {
+                    Object[] yvalues = new Object[2];
+                    yvalues[0] = h.TotalHours;
+                    yvalues[1] = tidIgjen.TotalHours;
+                    this.ChartHolder.Series["Brukte tid"].Points.AddXY(d.DayOfWeek + " " + d.ToShortDateString(), yvalues);
+                }
+                else
+                    this.ChartHolder.Series["Brukte tid"].Points.AddXY(d.DayOfWeek + " " + d.ToShortDateString(), tidIgjen.TotalHours);
+
+                this.ChartHolder.Series["Estimert tid"].Points.AddXY(d.DayOfWeek + " " + d.ToShortDateString(), estimat.TotalHours);
+            }
+        }
+    }
+}
